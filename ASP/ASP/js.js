90b2656e3c259272eb923c718ced91351ec06430
Pedro-Pezoa <pedroluizpezoa@gmail.com>
2016-11-17 10:13:45 -0200

    Merge da Master Page de funcionario e também a página de cadastro

---
+++ ASP/ASP/js.js
@@ -0,0 +1,6 @@
+﻿function login()
+{
+    divEnt = document.getElementById('item_menu6');
+    if (divEnt.style.visibility == 'hidden') divEnt.style.visibility = 'visible';
+    else divEnt.style.visibility = 'hidden';
+}
