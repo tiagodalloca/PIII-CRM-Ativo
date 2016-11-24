9e54d7cf3c70dfbd21750da621b0637aca8a7db5
CampioniMan <campioni.daniel@gmail.com>
2016-11-24 09:58:36 -0200

    Session em C#
    Agora há o esquema de Session para a parte de login do funcionário

--- ASP/ASP/CadastroCRM.aspx.cs
+++ ASP/ASP/CadastroCRM.aspx.cs
@@ -10,30 +10,49 @@ namespace ASP
 {
     public partial class CadastroCRM1 : System.Web.UI.Page
     {
+        // A conexão
         SqlConnection conn = new SqlConnection("Data Source=regulus;Initial Catalog=ERP;Persist Security Info=True;User ID=ERP;Password=ERP2016");
+        // vetores que relacionam-se com o BD
         static readonly string[] tabelas = { "SituacaoAtendimento", "TipoAtendimento", "SituacaoPedido", "CategoriaProduto", "CategoriaServico" };
         static readonly int[] lengths = { 40, 40, 40, 50, 50 };
 
         protected void btnCadSitAtend_Click(object sender, EventArgs e)
         {
-            if (txtTexto.Text.Length < lengths[cbxTabelas.SelectedIndex])
+            if (txtTexto.Text.Length < lengths[cbxTabelas.SelectedIndex]) // se tiver um tamanho adequado ao banco
             {
                 lblGrande.Visible = false;
+
+                // abre a conexão para certa tabela
                 conn.Open();
                 SqlCommand sql = new SqlCommand("INSERT INTO " + tabelas[cbxTabelas.SelectedIndex] + " VALUES(@desc)", conn);
                 sql.Parameters.AddWithValue("@desc", txtTexto.Text);
                 sql.ExecuteNonQuery();
+
+                // fecha a conexão
                 conn.Close();
                 txtTexto.Text = "";
             }
             else
-                lblGrande.Visible = true;
+                lblGrande.Visible = true; // mostra mensagem de erro
         }
 
         protected void btnLimpar1_Click(object sender, EventArgs e)
         {
             txtTexto.Text = "";
             lblGrande.Visible = false;
+        }
+
+        internal static bool isset(Object m)
+        {
+            return (@m == null) ? false : true; // @ indica literalmente("\n" é considerado como "\n" e não quebra de linha)
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!(isset(Session["NomeFunc"]) && isset(Session["SenhaFunc"]) && isset(Session["IDFunc"]))) // não tem nada na Session(não veio por Principal.Master)
+            {
+                Response.Redirect("index.aspx");
+            }
         }
     }
 }
--- ASP/ASP/Principal.Master.cs
+++ ASP/ASP/Principal.Master.cs
@@ -25,42 +25,57 @@ namespace ASP
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            if (txtNome.Text == "")
+            if (txtNome.Text == "") // nome não foi digitado
             {
                 tirarMensagens();
                 MNSI.Visible = true;
                 return;
             }
-            if (txtSenha.Text == "")
+            if (txtSenha.Text == "") // senha não foi digitada
             {
                 tirarMensagens();
                 MSI.Visible = true;
                 return;
             }
 
+            string nome = txtNome.Text;
+            string senha = txtSenha.Text;
+
+            // abrindo a conexão com o banco de dados
             SqlConnection conne = new SqlConnection("Data Source=regulus;Initial Catalog=ERP;Persist Security Info=True;User ID=ERP;Password=ERP2016");
             conne.Open();
-            SqlCommand sql = new SqlCommand("SELECT COUNT(*) FROM AcessoFuncionario WHERE loginFuncionario=@name and senhaFuncionario=@senha", conne);
-            sql.Parameters.AddWithValue("@name", txtNome.Text);
-            sql.Parameters.AddWithValue("@senha", txtSenha.Text);
+
+            // operando com o select, impedindo "SQL Injection"
+            SqlCommand sql = new SqlCommand("SELECT COUNT(*) as c, IdFunc as i FROM AcessoFuncionario WHERE loginFuncionario=@name and senhaFuncionario=@senha GROUP BY idFunc", conne);
+            sql.Parameters.AddWithValue("@name", nome);
+            sql.Parameters.AddWithValue("@senha", senha);
+
+            // Executando o select
             SqlDataReader dt = sql.ExecuteReader();
             if (dt.Read())
             {
-                if (dt.GetInt32(0) == 0)
+                if ((int)dt[0] == 0) // não tem no banco
                 {
                     tirarMensagens();
                     MUNSI.Visible = true;
                     return;
                 }
-                Response.Redirect("CadastroCRM.aspx");
+
                 // existe no BD
+                // armazena na Session(que nem PHP)
+                Session["IDFunc"] = (int)dt[1];
+                Session["NomeFunc"] = nome;
+                Session["SenhaFunc"] = senha;
+
+                // Muda de página
+                Response.Redirect("CadastroCRM.aspx");
             }
-            else
+            else // não tem dados (provavelmente erro de conexão)
             {
                 tirarMensagens();
                 MEC.Visible = true;
             }
-            dt.Close();
+            dt.Close(); // fechando as conexões
             conne.Close();
         }
     }
